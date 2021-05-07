c098e22ac0ef35aa25e6858c19585f09d2f862cd
Héctor Molinero Fernández <hector@molinero.dev>
2021-05-07 16:26:59 +0200

    Use dynamic resolution

--- xfreerdp.sh
+++ xfreerdp.sh
@@ -12,7 +12,7 @@ exec xfreerdp \
 	/v:"${RDP_HOST:?}":"${RDP_PORT:?}" \
 	/u:"${RDP_USER:?}" /p:"${RDP_PASSWORD:?}" \
 	/log-level:INFO /cert:ignore \
-	/gfx:RFX +gfx-progressive /w:1600 /h:900 \
+	/gfx:RFX +gfx-progressive /dynamic-resolution \
 	/audio-mode:0 /sound:sys:pulse,rate:44100 \
 	/microphone:sys:pulse,rate:44100 \
 	+clipboard +home-drive \
