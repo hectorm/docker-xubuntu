a0a0b44fdc1b7a57f5c0594fc2d7c57e331e9305
Héctor Molinero Fernández <hector@molinero.dev>
2025-06-13 01:09:39 +0200

    Add wlfreerdp script

---
+++ wlfreerdp.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -eu
+export LC_ALL=C
+
+RDP_HOST=127.0.0.1
+RDP_PORT=3389
+RDP_DOMAIN=Xorg
+RDP_USER=user
+RDP_PASSWORD=password
+
+exec wlfreerdp3 \
+	/v:"${RDP_HOST:?}":"${RDP_PORT:?}" \
+	/u:"${RDP_DOMAIN:?}"\\"${RDP_USER:?}" /p:"${RDP_PASSWORD:?}" \
+	/log-level:INFO /cert:ignore \
+	/gfx:AVC444v2 /bpp:32 /dynamic-resolution \
+	/audio-mode:0 /sound:sys:pulse,rate:44100 \
+	/microphone:sys:pulse,rate:44100 \
+	+clipboard +home-drive \
+	-compression -encryption
